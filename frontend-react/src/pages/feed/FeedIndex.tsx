aae023118bb6107af16b4ee23f271b32be33685b
dahoon06 <dahoon226@naver.com>
2023-01-26 11:38:49 +0900

    feat: feed header

--- frontend-react/src/pages/feed/FeedIndex.tsx
+++ frontend-react/src/pages/feed/FeedIndex.tsx
@@ -2,13 +2,14 @@ import { FeedHeader } from '@/components/feed/FeedHeader'
 import { Footer } from '@/components/common/footer/Footer'
 import { FeedLists } from '@/components/feed/FeedLists'
 import { RecommendedUser } from '@/components/feed/RecommendedUser'
+import style from '@/pages/feed/FeedIndex.module.scss'
 
 export const FeedIndex = () => {
   return (
     <>
       <FeedHeader />
       {/*<Outlet />*/}
-      <main>
+      <main className={style.feed__main}>
         <FeedLists />
         <RecommendedUser />
       </main>
