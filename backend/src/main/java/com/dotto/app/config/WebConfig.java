4b1601e3538fe680656f6b621708b0a1e8176e10
jaeeey01 <yjh920127@gmail.com>
2023-01-27 10:49:34 +0900

    chore : cors 관련 설정 - corsMapping - options.name 추가

--- backend/src/main/java/com/dotto/app/config/WebConfig.java
+++ backend/src/main/java/com/dotto/app/config/WebConfig.java
@@ -21,7 +21,8 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name());
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name());
     }
 
 }
